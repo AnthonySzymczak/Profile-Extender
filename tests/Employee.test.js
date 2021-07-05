6095667f22fe4214b0571d32b1d4e506c8f329fa
AnthonySzymczak <doyalajs1@live.com>
2021-07-05 16:30:33 -0400

    Comments added to  library .js files, next is Engineer.test.js

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,40 +1,52 @@
 const Employee = require('../lib/Employee');
 
-test('Publishes the employee object', () => {
+test('Publishes employee (as object)', () => {
     const employee = new Employee();
     expect(typeof(employee)).toBe("object");
 });
 
-test('PUB employee name', () => {
+// declares the employee's name
+test('employee name', () => {
     const name = 'Tony'
     const employee = new Employee(name);
     expect(employee.name).toBe(name);
 });
-test('PUB employee ID', () => {
-    const testId = 100;
+
+// declares the employee's id
+test('employee ID', () => {
+    const testId = 999;
     const employee = new Employee('Tony', testId);
     expect(employee.id).toBe(testId);
 });
-test('PUB employee e-mail', () => {
+
+// declares the employee's email
+test('employee e-mail', () => {
     const testEmail = 'anthonyszymczak@msn.com'
     const employee = new Employee('Tony', 1, testEmail);
     expect(employee.email).toBe(testEmail);
 });
-test('grab name from getName', () => {
+// retrieves employee's name
+test('getName', () => {
     const testName = 'Tony'
     const employee = new Employee(testName);
     expect(employee.getName()).toBe(testName);
 });
-test('grab ID from getId', () => {
-    const testId = '999'
+
+// retrieves employee's id
+test('getId', () => {
+    const testId = 999
     const employee = new Employee('Tony', testId);
     expect(employee.getId()).toBe(testId);
 });
-test('grab email from getEmail', () => {
+
+//retrieves employee's e-mail
+test('getEmail', () => {
     const testEmail = 'anthonyszymczak@msn.com'
     const employee = new Employee('Tony', 1, testEmail);
     expect(employee.getEmail()).toBe(testEmail);
 });
+
+// retrieves roll of employee
 test('getRole function', () => {
     const testRole = 'Employee'
     const employee = new Employee('Tony', 1, 'anthonyszymczak@msn.com');
